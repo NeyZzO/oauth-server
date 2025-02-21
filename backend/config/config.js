3605ac7de7546691be269912d8228f3771fc27f0
NeyZzO <66218417+NeyZzO@users.noreply.github.com>
2025-02-21 23:12:36 +0100

    Changed some config

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,14 +1,15 @@
 import { config } from "dotenv";
 config();
 
-const {DB_NAME, DB_HOST, DB_USER, DB_PASS, DB_DIALECT} = process.env;
+const {DB_NAME, DB_HOST, DB_USER, DB_PASS, DB_DIALECT, DB_PORT} = process.env;
 
 export default {
     development: {
+        dialect: DB_DIALECT,
         username: DB_USER,
         password: DB_PASS,
         host: DB_HOST,
         database: DB_NAME,
-        dialect: DB_DIALECT
+        port: DB_PORT
     }
 };
