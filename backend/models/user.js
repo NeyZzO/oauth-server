ef9cf0097d228b78eda5e9f8c4eb2c593fac8b11
NeyZzO <ioiottdago@gmail.com>
2025-02-21 09:58:55 +0100

    Added User model

---
+++ backend/models/user.js
@@ -0,0 +1,42 @@
+import {Model, DataTypes, Sequelize, DatabaseError} from "sequelize";
+
+class User extends Model {
+
+    /**
+     * 
+     * @param {Sequelize} sequelize 
+     */
+    static init(sequelize) {
+        super.init({
+            uuid: {
+                type: DataTypes.UUIDV4,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false, 
+                unique: true,
+                validate: {
+                    isEmail: true,
+                }
+            },
+            isVerified: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false,
+            },
+            twoFactorEnabled: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false,
+            },
+            twoFactorSecret: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            }
+        }, {
+            sequelize,
+            modelName: "User",
+            tableName: "users"
+        })
+    }
+}
