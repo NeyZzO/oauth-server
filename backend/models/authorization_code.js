9be8d24c77ad94b917ac4f4cfa7c40aed876ca27
NeyZzO <ioiottdago@gmail.com>
2025-02-26 14:38:49 +0100

    added linting and testing envs

---
+++ backend/models/authorization_code.js
@@ -0,0 +1,11 @@
+import {Model, DataTypes, Sequelize} from "sequelize";
+
+export default class AuthorizationCode extends Model {
+    /**
+     * 
+     * @param {Sequelize} sequelize 
+     */
+    static init(sequelize) {
+
+    }
+}
