95736306aa18955ec89964ee40bdde0f395ed081
NeyZzO <ioiottdago@gmail.com>
2025-02-27 12:00:42 +0100

    Changed Jest -> vitest

--- backend/tests/database.test.js
+++ backend/tests/database.test.js
@@ -1,12 +1,11 @@
-import { test } from "@jest/globals";
+import {test, describe, expect} from "vitest";
 import { sequelize } from "../models/index.js";
 
-test("Tests the database connection", async (done) => {
+test("Tests the database connection", async () => {
     try {
         const result = await sequelize.query("SELECT (1 + 1) AS RESULT;");
+        expect(result[0][0].result).toBe(2);
     } catch (err) {
         done (err);
     }        
-    console.log(result[0]);
-    done("Database connection successful");
 })
