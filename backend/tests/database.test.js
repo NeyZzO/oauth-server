d349e01f919cf938aa031d1cac5e1b6678e0be7c
NeyZzO <ioiottdago@gmail.com>
2025-02-27 12:06:09 +0100

    Fixed tests

--- backend/tests/database.test.js
+++ backend/tests/database.test.js
@@ -2,10 +2,6 @@ import {test, describe, expect} from "vitest";
 import { sequelize } from "../models/index.js";
 
 test("Tests the database connection", async () => {
-    try {
-        const result = await sequelize.query("SELECT (1 + 1) AS RESULT;");
-        expect(result[0][0].result).toBe(2);
-    } catch (err) {
-        done (err);
-    }        
-})
+    const result = await sequelize.query("SELECT (1 + 1) AS RESULT;");
+    expect(result[0][0].result).toBe(2);
+});
