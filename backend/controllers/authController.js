424d72f280e42dcb658b0f7efcfec539cdb58e76
Eliott <ioiottdago@gmail.com>
2025-03-19 21:55:49 +0100

    Adding user storage in database

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,12 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import {User, OAuthClient, OAuthToken} from "../models/index.js";
+import speakeasy from "speakeasy";
+import { User, OAuthClient, OAuthToken } from "../models/index.js";
+import PasswordProvider from "../providers/PasswordProvider.js";
+import chalk from "chalk";
 
 export default class AuthController {
-
     static isEmail(email) {
         // Email regex.
         const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -15,54 +17,122 @@ export default class AuthController {
         /// Data validation
         const { email, username, password, confirmPassword } = req.body;
         if (!email || !username || !password) {
-            return res.status(400).json({ message: "All fields are required", type: email ? (username ? "password" : "username") : "email" });
+            return res
+                .status(400)
+                .json({
+                    message: "All fields are required",
+                    type: email
+                        ? username
+                            ? "password"
+                            : "username"
+                        : "email",
+                });
         }
-        
+
         if (!AuthController.isEmail(email)) {
-            return res.status(400).json({ message: "Invalid email format", type: "email" });
+            return res
+                .status(400)
+                .json({ message: "Invalid email format", type: "email" });
         }
 
         /// Now do the checks on the password
         if (password.length < 8) {
-            return res.status(400).json({ message: "Password must be at least 8 characters long", type: "password" });
+            return res
+                .status(400)
+                .json({
+                    message: "Password must be at least 8 characters long",
+                    type: "password",
+                });
         }
 
         // Check if password contains at least one number
         if (!/\d/.test(password)) {
-            return res.status(400).json({ message: "Password must contain at least one number", type: "password" });
+            return res
+                .status(400)
+                .json({
+                    message: "Password must contain at least one number",
+                    type: "password",
+                });
         }
 
         // Check if password contains at least one uppercase letter
         if (!/[A-Z]/.test(password)) {
-            return res.status(400).json({ message: "Password must contain at least one uppercase letter", type: "password" });
+            return res
+                .status(400)
+                .json({
+                    message:
+                        "Password must contain at least one uppercase letter",
+                    type: "password",
+                });
         }
 
         // Check if password contains at least one lowercase letter
         if (!/[a-z]/.test(password)) {
-            return res.status(400).json({ message: "Password must contain at least one lowercase letter", type: "password" });
+            return res
+                .status(400)
+                .json({
+                    message:
+                        "Password must contain at least one lowercase letter",
+                    type: "password",
+                });
         }
 
         // Check if password contains at least one special character
         if (!/[!@#$%^&*]/.test(password)) {
-            return res.status(400).json({ message: "Password must contain at least one special character", type: "password" });
+            return res
+                .status(400)
+                .json({
+                    message:
+                        "Password must contain at least one special character",
+                    type: "password",
+                });
         }
 
-        if(password != confirmPassword) {
-            return res.status(400).json({ message: "Passwords do not match", type: "confirmPassword" });
+        if (password != confirmPassword) {
+            return res
+                .status(400)
+                .json({
+                    message: "Passwords do not match",
+                    type: "confirmPassword",
+                });
         }
-
 
         // Check if username and email are already in use
-        const userWithUsername = await User.findOne({ where: {username} });
-        if (userWithUsername) return res.status(400).json({ message: "Username already in use", type: "username" });
-        const userWithEmail = await User.findOne({where: {
-            email
-        }});
-        if(userWithEmail) return res.status(400).json({message: "Email already in use", type: "email"});
+        const userWithUsername = await User.findOne({ where: { username } });
+        if (userWithUsername)
+            return res
+                .status(400)
+                .json({ message: "Username already in use", type: "username" });
+        const userWithEmail = await User.findOne({
+            where: {
+                email,
+            },
+        });
+        if (userWithEmail)
+            return res
+                .status(400)
+                .json({ message: "Email already in use", type: "email" });
 
+        // * All the data is valid, we can now put everything in the database;
+        try {
+            const hashed = PasswordProvider.hash(password);
 
-        res.status(201).json({message: "Created"});
-
+            const user = await User.create({
+                email,
+                username,
+                password: hashed,
+                isVerified: true,
+                twoFactorEnabled: true,
+                twoFactorSecret: speakeasy.generateSecret().base32,
+            });
+            
+            // TODO: Create session and store the UUID inside
+            
+            return res.status(201).json({ message: "Created" });
+        } catch (err) {
+            console.error(chalk.red(err.message));
+            res.return(500).json({ message: "Unknown error" });
+        }
     }
 
     static async login(req, res) {
--- backend/providers/PasswordProvider.js
+++ backend/providers/PasswordProvider.js
@@ -1,5 +1,30 @@
-import bcrypt from "bcryptjs";
+import argon2 from "@node-rs/argon2";
+import dotenv from "dotenv";
+dotenv.config();
+const { APP_PASSWORD_SECRET } = process.env;
 
 class PasswordProvider {
-    
+    /**
+     * Returns a hashed password using argon2 algorithm
+     * @param {string} pass
+     * @returns {string} 
+     */
+    static async hash(pass) {
+        const hashed = await argon2.hash(pass, {
+            secret: APP_PASSWORD_SECRET
+        });
+        return hashed;
+    }
+
+    /**
+     * Returns whether or not the password corresponds to the given hash
+     * @param {string} pass 
+     * @param {string} hash
+     * @returns {boolean}
+     */
+    static async verify(pass, hash) {
+        return (await argon2.verify(hash, pass, {secret: APP_PASSWORD_SECRET}));
+    }
 }
+
+export default PasswordProvider;
